887e65f384d6d6ae112c4ab9fe7cede753e079bf
Henrique Paes de Souza <henriquee.paes1@gmail.com>
2023-04-13 01:09:39 -0300

    spline final

--- spline.py
+++ spline.py
@@ -25,18 +25,74 @@ def criar_spline(x, y, y_compara, caso):
         
         case 2:
             plt.figure(figsize = (10,8))
-            plt.plot(x_new, y_new, 'b', label='Interpolação polinomial para o modelo completo') # plota o resultado do spline na cor azul
+            plt.plot(x_new, y_new, 'r', label='Interpolação polinomial para o modelo completo') # plota o resultado do spline na cor azul
             plt.plot(x, y, 'ro', label='Dados tabelados para o modelo completo') # plota os pontos correspondentes aos x e y anteriores
             plt.plot(x, y_compara, 'go', label='Dados tabelados do caso simplificado') # plota os pontos correspondentes aos x e y anteriores
-            plt.title("Comparação dos modelos através de interpolação polinomial para a velocidade do projétil")
+            plt.title("Comparação dos modelos através de interpolação polinomial para a velocidade no eixo x do projétil")
             plt.xlabel("Tempo", labelpad=10)
             plt.ylabel("Velocidade em x", labelpad=10)
             plt.plot()
             plt.legend(loc="lower left")
             plt.savefig("spline_velocidade.png")
 
+def spline_eixos(t, x_simples, x_completo, y_simples, y_completo, z_simples, z_completo):
+    x_simples_spline = CubicSpline(t, x_simples) # cria o spline
+    y_simples_spline = CubicSpline(t, y_simples) # cria o spline 
+    z_simples_spline = CubicSpline(t, z_simples) # cria o spline
+    x_completo_spline = CubicSpline(t, x_completo) # cria o spline
+    y_completo_spline = CubicSpline(t, y_completo) # cria o spline 
+    z_completo_spline = CubicSpline(t, z_completo) # cria o spline
+    t_new = np.linspace(t[0], t[-1]) # cria um vetor com os mesmos extremos em x, mas com menos espaco entre os dados, com o intuito de visualizar mais pontos
+
+
+    x_simples_new = x_simples_spline(t_new) # o novo vetor para y deve ser todos os pontos de x calculados utilizando o spline feito, substancialmente y_new = S(x_new), para os pontos dados
+    y_simples_new = y_simples_spline(t_new)
+    z_simples_new = z_simples_spline(t_new)
+
+    x_completo_new = x_completo_spline(t_new)
+    y_completo_new = y_completo_spline(t_new)
+    z_completo_new = z_completo_spline(t_new)
+
+    plt.figure(figsize = (10,8))
+    plt.plot(t_new, x_completo_new, 'r', label='Interpolação polinomial para o modelo completo') # plota o resultado do spline na cor azul
+    plt.plot(t, x_completo, 'ro', label='Dados tabelados para o modelo completo') # plota os pontos correspondentes aos x e y anteriores
+    plt.plot(t, x_simples, 'go', label='Dados tabelados do caso simplificado') # plota os pontos correspondentes aos x e y anteriores
+    plt.plot(t_new, x_simples_new, 'g', label='Interpolação polinomial para o modelo simplificado') # plota o resultado do spline na cor azul
+    plt.title("Comparação dos modelos através de interpolação polinomial para a posição do projétil em x")
+    plt.xlabel("Tempo", labelpad=10)
+    plt.ylabel("Posição em x", labelpad=10)
+    plt.plot()
+    plt.legend(loc="upper left")
+    plt.savefig("spline_x.png")
+
+    plt.figure(figsize = (10,8))
+    plt.plot(t_new, y_completo_new, 'r', label='Interpolação polinomial para o modelo completo') # plota o resultado do spline na cor azul
+    plt.plot(t, y_completo, 'ro', label='Dados tabelados para o modelo completo') # plota os pontos correspondentes aos x e y anteriores
+    plt.plot(t, y_simples, 'go', label='Dados tabelados do caso simplificado') # plota os pontos correspondentes aos x e y anteriores
+    plt.plot(t_new, y_simples_new, 'g', label='Interpolação polinomial para o modelo simplificado') # plota o resultado do spline na cor azul
+    plt.title("Comparação dos modelos através de interpolação polinomial para a posição do projétil em y")
+    plt.xlabel("Tempo", labelpad=10)
+    plt.ylabel("Posição em y", labelpad=10)
+    plt.plot()
+    plt.legend(loc="upper left")
+    plt.savefig("spline_y.png")
+
+    plt.figure(figsize = (10,8))
+    plt.plot(t_new, z_completo_new, 'r', label='Interpolação polinomial para o modelo completo') # plota o resultado do spline na cor azul
+    plt.plot(t, z_completo, 'ro', label='Dados tabelados para o modelo completo') # plota os pontos correspondentes aos x e y anteriores
+    plt.plot(t, z_simples, 'go', label='Dados tabelados do caso simplificado') # plota os pontos correspondentes aos x e y anteriores
+    plt.plot(t_new, z_simples_new, 'g', label='Interpolação polinomial para o modelo simplificado') # plota o resultado do spline na cor azul
+    plt.title("Comparação dos modelos através de interpolação polinomial para a posição do projétil em z")
+    plt.xlabel("Tempo", labelpad=10)
+    plt.ylabel("Posição em z", labelpad=10)
+    plt.plot()
+    plt.legend(loc="lower left")
+    plt.savefig("spline_z.png")
+
 def valores_simplificado():
-    alturas_simplificado = []
+    x_simplificado = []
+    y_simplificado = []
+    z_simplificado = []
     velocidadex_simplificado = []
     tempo = []
     t_0 = 0
@@ -62,15 +118,19 @@ def valores_simplificado():
             t = t_0
             y_t = y_0
         tempo.append(t)
-        alturas_simplificado.append(y_t[2])
+        x_simplificado.append(y_t[0])
+        y_simplificado.append(y_t[1])
+        z_simplificado.append(y_t[2])
         velocidadex_simplificado.append(y_t[3])
         i+=1
 
-    return tempo, alturas_simplificado, velocidadex_simplificado
+    return tempo, x_simplificado, y_simplificado, z_simplificado, velocidadex_simplificado
 
 def valores_completo():
     tempo = []
-    alturas_vento_resist = []
+    x_vento_resist = []
+    y_vento_resist = []
+    z_vento_resist = []
     velocidadex_vento_resist = []
 
     coef_arrasto = 0.35
@@ -103,16 +163,19 @@ def valores_completo():
             t = t_0
             y_t = y_0
         tempo.append(t)
-        alturas_vento_resist.append(y_t[2])
+        x_vento_resist.append(y_t[0])
+        y_vento_resist.append(y_t[1])
+        z_vento_resist.append(y_t[2])
         velocidadex_vento_resist.append(y_t[3])
         i+=1
 
-    return tempo, alturas_vento_resist, velocidadex_vento_resist
+    return tempo, x_vento_resist, y_vento_resist, z_vento_resist, velocidadex_vento_resist
 
 def main():
-    t, alturas, velocidade = valores_simplificado()
-    t, alturas_completo, velocidade_completo = valores_completo()
-    criar_spline(t, alturas, velocidade, 1)
+    t, x_simples, y_simples, z_simples, velocidade = valores_simplificado()
+    t, x_completo, y_completo, z_completo, velocidade_completo = valores_completo()
+    # criar_spline(t, x, y, velocidade, 1)
     criar_spline(t, velocidade_completo, velocidade, 2)
+    spline_eixos(t, x_simples, x_completo, y_simples, y_completo, z_simples, z_completo)
 
 main()
